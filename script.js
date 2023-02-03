c65df175c1af10b4cf451d4f9decbc9f961d4f1d
Soliha <solyatursuntasheva@gmail.com>
2023-02-04 02:08:39 +0500

    Finish!

--- script.js
+++ script.js
@@ -1,40 +1,57 @@
-/* form validation */
+let exclamation = document.getElementsByClassName("error");
+let form = document.getElementById("form");
+let alert = document.getElementById("alert");
+let firstName = document.getElementById("firstName");
+let lastName = document.getElementById("lastName");
+let email = document.getElementById("email");
+let password = document.getElementById("password");
+let formError = document.getElementsByClassName("form_error");
+let inputs = document.getElementsByTagName("input");
 
-var exclamation = document.getElementsByClassName("error");
-var form = document.getElementById("form");
-var firstName = document.getElementById("firstName");
-var lastName = document.getElementById("lastName");
-var email = document.getElementById("email");
-var password = document.getElementById("password");
-var formError = document.getElementsByClassName("form_error");
-var input = document.getElementsByTagName("input");
-
-/* validation*/
+/* validation */
 function formValidation(event) {
-  /*for loop for all input fields except submit input, so it is 4*/
-  for (i = 0; i < 4; i++) {
-    /*clear the form*/
-    input[i].classList.remove("error");
+  let formValid = true;
+
+  /* for loop for all input fields except submit input, so it is 4 */
+  for (i = 0; i < inputs.length - 1; i++) {
+    /* clear the form */
+    inputs[i].classList.remove("error");
     formError[i].style.display = "none";
 
-    /*email validation*/
-    if (!email.validity.valid) {
+    /* email validation */
+    if (inputs[i].type === "email" && !inputs[2].validity.valid) {
       formError[2].style.display = "block";
-      email.classList.add("error");
-      event.preventDefault();
+      inputs[2].classList.add("error");
+      formValid = false;
     }
 
-    /*validation of other fields*/
+    /* validation of other fields */
     if (
-      (input.type != "email" || input.type != "submit") &&
-      input[i].value == ""
+      inputs[i].type !== "email" &&
+      inputs[i].type !== "submit" &&
+      inputs[i].value === ""
     ) {
       formError[i].style.display = "block";
-      input[i].classList.add("error");
-      event.preventDefault();
+      inputs[i].classList.add("error");
+      formValid = false;
     }
+  }
+
+  if (
+    firstName.value &&
+    lastName.value &&
+    email.value &&
+    password.value &&
+    formValid
+  ) {
+    alert.style.display = "block";
+    setTimeout(function () {
+      alert.style.display = "none";
+    }, 100000);
+  } else {
+    event.preventDefault();
   }
 }
 
-/*event listener on button, call the formValidation function*/
+/* event listener on button, call the formValidation function */
 form.addEventListener("submit", formValidation, false);
