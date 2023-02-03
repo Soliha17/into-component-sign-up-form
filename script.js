7f42d8f8835596d16e60a7792190f5e2617b403d
Soliha <solyatursuntasheva@gmail.com>
2023-02-03 23:04:48 +0500

    Finish

--- script.js
+++ script.js
@@ -9,29 +9,6 @@ var password = document.getElementById("password");
 var formError = document.getElementsByClassName("form_error");
 var input = document.getElementsByTagName("input");
 
-// function formValidation(event) {
-//   for (i = 0; i < 4; i++) {
-//     input[i].classList.remove("error");
-//     formError[i].style.display = "none";
-
-//     if (!email.validity.valid) {
-//       formError[2].style.display = "block";
-//       email.classList.add("error");
-//       event.preventDefeult();
-//     }
-
-//     if (
-//       (input.type != "email" || input.type != "submit") &&
-//       input[i].value == ""
-//     ) {
-//       formError[i].style.display = "block";
-//       input[i].classList.add("error");
-//       event.preventDefeult();
-//     }
-//   }
-// }
-
-// form.addEventListener("submit", formValidation, false);
 
 /* validation*/
 function formValidation(event) {
